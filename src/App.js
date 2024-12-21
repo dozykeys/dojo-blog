2ab96e485fcd2ebaaaa7be7bc2830b693e927621
Duru Chidozie <chidozieduru@gmail.com>
2024-12-21 23:22:10 +0000

    feat: :zap: add blogs and improve css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  // const likes = {50};
+ 
   return (
     <div className="App">
       <Navbar />
