691a98ed7f3bb6709430b6a457ea47210587574e
Duru Chidozie <chidozieduru@gmail.com>
2024-12-21 22:25:30 +0000

    feat: :sparkles: added button click features

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  function handleClick(e,b) {
+    console.log(e,b.target);
+  }
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={(e)=>handleClick("dozie",e)}>Yes</button>
     </div>
   );
 };
