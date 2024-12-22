e7cd6d7093c80aa2b83717bd77582087a7595d55
Duru Chidozie <chidozieduru@gmail.com>
2024-12-22 23:26:20 +0000

    add blog page

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList
+ = ({blogs,title}) => {
+    return ( 
+        <div className="blog-list">
+            <h1>{title}</h1>
+             {blogs.map(({ id ,title,author}) => (
+        <div className="blog-preview" key={id}>
+          <h2>{title}</h2>
+          <p>Written by {author}</p>
+        </div>
+      ))}
+        </div>
+     );
+}
+ 
+export default BlogList
+;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -12,15 +13,9 @@ const Home = () => {
     },
   ]);
 
-
   return (
     <div className="home">
-      {blogs.map(({ id ,title,author}) => (
-        <div className="blog-preview" key={id}>
-          <h2>{title}</h2>
-          <p>Written by {author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!"/>
     </div>
   );
 };
