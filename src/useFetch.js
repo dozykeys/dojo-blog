cc878dd21d67b474792ae972004721b8635943fd
Duru Chidozie <chidozieduru@gmail.com>
2024-12-31 00:50:12 +0000

    add useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,35 +1,33 @@
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from "react";
 
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-const useFetch =(url)=>{
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+    return () => console.log("program finished");
+  }, [url]);
 
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-          fetch(url)
-            .then((res) => {
-              if (!res.ok) {
-                throw Error("Could not fetch the data for that resource");
-              }
-              return res.json();
-            })
-            .then((data) => {
-              setData(data);
-              setIsPending(false);
-              setError(null);
-            })
-            .catch((err) => {
-              setError(err.message);
-              setIsPending(false);
-            });
-        }, 1000);
-      }, [url]);
-
-      return {data,isPending,error}
-}
-
+  return { data, isPending, error };
+};
 
 export default useFetch;
